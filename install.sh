b2a253af27709902744c3e6418f174b957e85de0
gerblesh <101901964+gerblesh@users.noreply.github.com>
2022-10-02 23:42:19 -0700

    made less cringe

--- install.sh
+++ install.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/bash
 ROOT_UID=0
-
+# if root install in root /bin folder
 if [ "$UID" -eq "$ROOT_UID" ]; then
   DEST_DIR="/usr"
 else
   DEST_DIR="$HOME/.local"
 fi
 
-cp  "$PWD/programs/"*  "$DEST_DIR/bin"
+# symlinking each file in the sounds directory
+
+cp "$PWD/bruh" "$HOME/.local/bin/bruh"
+echo "copied bruh into $HOME/.local/bin"
+FILES="$PWD/sounds/"*
+for f in $FILES do
+  FILEPATH=$HOME/.local/bin/$(echo $f |awk -F/ '{print $NF}' |  awk -F. '{print $1}')
+   if [  -e $FILEPATH ]; then 
+        rm $FILEPATH # file exists, remove it to prevent failure for ln
+   fi
+  ln -s "$HOME/.local/bin/bruh" $FILEPATH
+done
+
 cp "$PWD/sounds/"*  "$HOME/.local/share"
-echo "copied programs to $DEST_DIR/bin"
+echo "symlinked sound programs to $HOME/.local/bin"
 echo "copied sounds to $HOME/.local/share"
